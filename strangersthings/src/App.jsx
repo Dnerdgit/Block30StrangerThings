e33b749327739932f39c33a88a427d8b6b16377a
elsa <elsa92gl@gmail.com>
2023-08-18 05:54:01 -0500

    form app jsx changes

--- strangersthings/src/App.jsx
+++ strangersthings/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import PostView from './components/PostView'
+import { useState } from "react";
+import "./App.css";
+import PostView from "./components/PostView";
+import Login from "./components/LoginForm";
+import SingUp from "./components/SignUpForm";
 
 function App() {
+  const [currentForm, currentSetForm] = useState("login");
+
+  const toggleForm = (formName) => {
+    currentSetForm(formName);
+  };
 
   return (
     <>
-      <PostView/>
+      {" "}
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <SingUp onFormSwitch={toggleForm} />
+      )}
+      <PostView />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

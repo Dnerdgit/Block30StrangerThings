5b4b9b0a75d45f96618516da9982246b6c0d7314
DuBoris Smith <duboris.smith@hotmail.com>
2023-08-21 01:53:16 -0500

    Copied/Deleted old post file. Added LocalStorage code. Added useLocalStorage file via hooks

--- strangersthings/src/components/CreateAccount.jsx
+++ strangersthings/src/components/CreateAccount.jsx
@@ -1,111 +1,115 @@
 import { useState, useEffect } from 'react'
-import { RegisterSignInData } from "../API";
-import { useNavigate } from 'react-router-dom';
+// import { RegisterSignInData } from "../API";
+// import useLocalStorage from '../hooks/useLocalStorage'
+// import { useNavigate } from 'react-router-dom';
 
-export default function CreateAccount({ posted, setPosted }) {
+export default function CreateAccount() {
 
- //     const getUser = JSON.parse(localStorage.getItem("user"))
- //     const getPass = JSON.parse(localStorage.getItem("pass"))
+        const [username, setUsername] = useState(() => {
+            const savedUser = localStorage.getItem("Username");
+            const parsedUser = JSON.parse(savedUser);
+            return parsedUser || "";
+        });
+        const [password, setPassword] = useState(() => {
+            const savedPass = localStorage.getItem("Password");
+            const parsedPass = JSON.parse(savedPass);
+            return parsedPass || "";
+        });
+        const [confirmPass, setConfirmPass] = useState("");
+        const [error, setError] = useState(null);
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [acceptPass, setAcceptPass] = useState("");
-    const [error, setError] = useState(null);
+        // const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-   
-
-
-    useEffect(() => {
-        // console.log(username);
-        // console.log(password);
-        localStorage.setItem('user', JSON.stringify(username));
-        localStorage.setItem('pass', JSON.stringify(password));
-        localStorage.setItem('aPass', JSON.stringify(acceptPass));
-
-        }, [username, password]
-        
-     )
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-
-      if (password !== setAcceptPass) {
-        return;
-      }
-      
-      const newProfile = await RegisterSignInData(username, password, acceptPass);
-        
-      console.log(newProfile);
-      if (newProfile.success) {
+    
 
 
-        console.log("New User: ", newProfile.data.posts);
+        useEffect(() => {
+            localStorage.setItem('Username', JSON.stringify(username));
+            localStorage.setItem('Password', JSON.stringify(password));
+            localStorage.setItem('Confirm', JSON.stringify(confirmPass));
 
-        const newProfileList = [...posted, newProfile.data.posts];
-        setPosted(newProfileList);
-
-        setUsername(username);
-        setPassword(password);
-        setAcceptPass(acceptPass);
-      } else {
-        setError(error);
-      }
-   }
-  return (
-
-    <div className="signInApp">
-        {error && <p>{error}</p>}
-        <h2>Create Account</h2>
-        <form onSubmit={handleSubmit} className='sign-up-form'>
-            <label>
-                Username 
-            </label>
-            <input 
-                id="name"
-                type="text"
-                name="name"
-                value={username}
-                placeholder="Username"
-                onChange={(e) => setUsername(e.target.value)}
-                />
+            }, [username, password]
             
-                <br/>
-                <br/>
-            <label>
-                Password 
-            </label>
-            <input
-                id="password"
-                type="password"
-                name="password"
-                value={password}
-                placeholder="**********"
-                onChange={(e) => setPassword(e.target.value)}
-                />
+        )
+
+        const handleSubmit = async (event) => {
+        event.preventDefault();
+
+    //     const newProfile = await RegisterSignInData(username, password, acceptPass);
+
+    //     if (password !== setAcceptPass) {
+    //         return;
+    //     }
             
-                <br/>
-                <br/>
-            <label>
-                Confirm Password
-            </label>
-            <input
-                id="acceptPass"
-                type="password"
-                name="acceptPass"
-                value={acceptPass}
-                placeholder="**********"
-                onChange={(e) => setAcceptPass(e.target.value)}
-                />
+    //     console.log(newProfile);
+    //     if (newProfile.success) {
 
-                <br/>
-                <br/>
-                <button onClick={(() => (setAcceptPass(password) ))}>Continue</button>
-        </form>
-    </div>
 
-    )
-}
+    //         console.log("New User: ", newProfile.data.posts);
+
+    //         const newProfileList = [...posted, newProfile.data.posts];
+    //         setPosted(newProfileList);
+
+    //         setUsername(username);
+    //         setPassword(password);
+    //         setAcceptPass(acceptPass);
+    //     } else {
+    //         setError(error);
+    //     }
+    }
+    return (
+
+        <div className="signInApp">
+            {error && <p>{error}</p>}
+            <h2>Create Account</h2>
+            <form onSubmit={handleSubmit} className='sign-up-form'>
+                <label>
+                    Username 
+                </label>
+                <input 
+                    id="name"
+                    type="text"
+                    name="name"
+                    value={username}
+                    placeholder="Username"
+                    onChange={(event) => setUsername(event.target.value)}
+                    />
+                
+                    <br/>
+                    <br/>
+                <label>
+                    Password 
+                </label>
+                <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="**********"
+                    onChange={(event) => setPassword(event.target.value)}
+                    />
+                
+                    <br/>
+                    <br/>
+                <label>
+                    Confirm Password
+                </label>
+                <input
+                    id="confirmPass"
+                    type="password"
+                    name="confirmPass"
+                    value={confirmPass}
+                    placeholder="**********"
+                    onChange={(event) => setConfirmPass(event.target.value)}
+                    />
+
+                    <br/>
+                    <br/>
+                    <button onClick={(() => (setConfirmPass(password) ))}>Submit</button>
+            </form>
+        </div>
+
+        )
+    }
 
 // (setPassword === password) ? 'Login Accepted' : password

0cd15d586009d7f08e4162329722dd21ea36ea88
Ajach <jasonajach90@gmail.com>
2023-08-21 19:24:38 -0500

    minor changes to post view, added Add Post button

--- strangersthings/src/components/PostView.jsx
+++ strangersthings/src/components/PostView.jsx
@@ -1,18 +1,29 @@
 //IMPORT FETCH POSTS FUNCTION and required hooks
 import fetchPosts from "../API/apiurl";
 import { useState, useEffect } from "react";
+import { useNavigate ,useParams } from "react-router-dom";
+
 
 export default function PostView() {
     //using useState hook, create variables to hold posts and set players
     const [posts, setPosts] = useState([]);
-    const [searchPost, setSearchPost] = useState(""); //store search words in the post search bar in this state
+    const [searchPost, setSearchPost] = useState(""); //store searched words in the post search bar in this state
+    const newPostNav = useNavigate(); //navigate to add new post form
+    const { id } = useParams; //getId post id
 
     //Matched a post to the search bar word
     function postSame(post, word) {
         //return true if any of the fields include the text
+        if(word && word.trim().length > 0) {
+            word = word.trim().toLowerCase();
+
+            //shows all posts when search bar is empty, otherwise, display the matching fields
+            return word === '' ? post : post.title.toLowerCase().includes(word);
+        }
     }
 
     const filteredPosts = posts.filter((post) => postSame(post, searchPost));
+    //if there is a text in the search bar, show filteredPosts else show all posts 
     const postsToShow = searchPost.length ? filteredPosts : posts;
 
     //call useEffect hook 
@@ -38,34 +49,31 @@ export default function PostView() {
     <>
             {/* FORM TO SEARCH FOR A POST */}
             <div className="posts-navbar">
-                <form>
-                    <label className="search-form-label">
-                        <h2> Posts </h2> 
-                        <input className="posts-search-bar" value={searchPost} onChange={(e) => setSearchPost(e.target.value)} placeholder="Search Posts"  /> 
-                    </label><br/>
-                </form>
+                <h1> Posts </h1> 
+                <input className="posts-search-bar" value={searchPost} onChange={(e) => setSearchPost(e.target.value)} placeholder="Search Posts"  />
+                <button className="add-post" /* onClick={ () => newPostNav(`/posts/${}`) } */> Add Post </button>  
             </div>
 
-        {/* MAP OVER THE POSTS ARRAY AND RENDER IT */}
-        <div className="posts-container"> 
-            { /* start of js */
+            {/* MAP OVER THE POSTS ARRAY AND RENDER IT */}
+            <div className="posts-container"> 
+                { /* start of js */
 
-                postsToShow.map((post, key) => {
-                    //return this div
-                    return (
-                        <div key={key} className="post">
-                            <h2> {post.title} </h2>
-                            <p> {post.description} </p>
-                            <p id="post-keys">Price: {post.price} </p>
-                            <p id="post-keys"> Seller: {post.author.username} </p>
-                            <p id="post-keys"> Location: {post.location} </p>
+                    postsToShow.map((post, key) => {
+                        //return this div
+                        return (
+                            <div key={key} className="post">
+                                <h2> {post.title} </h2>
+                                <p> {post.description} </p>
+                                <p id="post-keys">Price: {post.price} </p>
+                                <p id="post-keys"> Seller: {post.author.username} </p>
+                                <p id="post-keys"> Location: {post.location} </p>
 
-                        </div>
-                    )
-                }) 
-                /* end of js */
-            } 
-        </div>
+                            </div>
+                        )
+                    }) 
+                    /* end of js */
+                } 
+            </div>
 
     </>
     )

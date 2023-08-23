fa3fa8eaac6da067c2c783ddc4de0b6dda2988a6
DuBoris Smith <duboris.smith@hotmail.com>
2023-08-23 01:53:04 -0500

    Created Sign Out button function

--- strangersthings/src/components/Authenticate.jsx
+++ strangersthings/src/components/Authenticate.jsx
@@ -1,6 +1,19 @@
 import { AuthProvider } from 'react-auth-kit'
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState } from 'react'
+import { useLocalStorage } from '../hooks/useLocalStorage'
+
+
 
 export function AuthProvider({ children }) {
-    
+    const [isSignedIn, setIsSignedIn] = useState(useLocalStorage(
+        localStorage.getItem("Username") && localStorage.getItem("Password")
+        )
+    );
+
+    const handleAuth = (signedIn) => {
+        setIsSignedIn(signedIn);
+    };
+    return <AuthContext.Provider value ={{ isSignedIn, handleAuth}}>
+        {children}
+    </AuthContext.Provider>
 }
--- strangersthings/src/components/SignOut.jsx
+++ strangersthings/src/components/SignOut.jsx
@@ -1,15 +1,26 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useAuth } from './Authenticate';
 
-export default function SignOut() {
+const SignOutButton = () => {
     const navigate = useNavigate();
-
+    const { logout } = useAuth();
+   
     useEffect(() => {
         localStorage.removeItem('Username');
         localStorage.removeItem('Password');
-        localStorage.removeItem('Confirm');
+
+        localStorage.setItem("isSignedIn", false);
+        logout(false);
 
         navigate("/");
 
-    },[])
-};
+    },[logout, navigate])
+
+    return (
+
+        <button onClick={logout}>Sign Out</button>
+    )
+}
+
+export default SignOutButton;

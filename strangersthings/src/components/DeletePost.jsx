3aefc5fde072d0ebf29d14bcde49e8176f89e2c5
McKinley Blezin <mblezin.usca@gmail.com>
2023-08-24 17:29:49 -0400

    added delete component

---
+++ strangersthings/src/components/DeletePost.jsx
@@ -0,0 +1,19 @@
+const deletePost = async (token) => {
+  try {
+    const response = await fetch(
+      `https://strangers-things.herokuapp.com/api/2302-acc-ct-web-pt-a/`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
